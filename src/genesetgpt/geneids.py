986881ca1d9035a0784bc115449905686d743461
Jack Leary <j.leary@ufl.edu>
2025-06-16 04:35:19 -0400

    added gene ID fetching fn

---
+++ src/genesetgpt/geneids.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from pybiomart import Server
+
+def fetch_gene_table(sort_by: str = 'hgnc_symbol') -> pd.DataFrame: 
+    """
+    Fetch a table of gene IDs and other per-gene metadata. 
+
+    Parameters
+    ----------
+    sort_by : str 
+        A string specifying the gene ID to sort the table by. Defaults to 'hgnc_symbol'. 
+
+    Returns
+    -------
+    gene_df : pd.DataFrame 
+        A DataFrame containing gene IDs, biotypes, descriptions, and chromosomal locations. 
+    """
+    server = Server(host='http://www.ensembl.org')
+    mart = server.marts['ENSEMBL_MART_ENSEMBL']
+    dataset = mart.datasets['hsapiens_gene_ensembl']
+    gene_df = dataset.query(
+        attributes=[
+            'ensembl_gene_id', 
+            'entrezgene_id', 
+            'hgnc_symbol', 
+            'description', 
+            'gene_biotype', 
+            'chromosome_name', 
+            'start_position', 
+            'end_position'
+        ],
+        only_unique=True
+    )
+    gene_df.rename(
+        columns={
+            'Gene stable ID': 'ensembl_id', 
+            'HGNC symbol': 'hgnc_symbol', 
+            'NCBI gene (formerly Entrezgene) ID': 'entrez_id', 
+            'Gene description': 'description', 
+            'Gene type': 'biotype', 
+            'Chromosome/scaffold name': 'chromosome', 
+            'Gene start (bp)': 'location_start', 
+            'Gene end (bp)': 'location_end'
+        }, 
+        inplace=True
+    )
+    gene_df['entrez_id'] = gene_df['entrez_id'].map(lambda x: f'{x:.0f}').astype(str)
+    gene_df['description'] = gene_df['description'].str.replace(r'\s*\[Source:[^\]]*\]', '', regex=True)
+    gene_df.replace(
+        to_replace=['nan', 'None'],
+        value=np.nan,
+        inplace=True
+    )
+    if sort_by not in gene_df.columns: 
+        raise KeyError(f"`sort_by='{sort_by}'` is not a valid column.")
+    gene_df.sort_values(by=sort_by, inplace=True)
+    return gene_df 
