87686a4a56b69be29c1b02e55d9d8c6bda1c84ae
Jack Leary <j.leary@ufl.edu>
2026-06-06 05:43:51 -0400

    fixed a link typo in the datasets file

--- src/genesetgpt/datasets.py
+++ src/genesetgpt/datasets.py
@@ -2,13 +2,13 @@ import importlib.resources as pkg_resources
 
 def load_example_gene_set():
     """
-    Loads a list of B-cell marker genes generated from the widely-used `10X Genomics PBM3k scRNA-seq dataset`_.
+    Loads a list of B-cell marker genes generated from the widely-used `10X Genomics PBMC3k scRNA-seq dataset`_.
     
     Returns
     -------
         A list of HGNC symbols representing the 50 most-significant marker genes by Wilcoxon test for the B-cell cluster. 
 
-    .. _10X Genomics PBM3k scRNA-seq dataset: https://scanpy.scverse.org/en/stable/tutorials/basics/clustering-2017.html
+    .. _10X Genomics PBMC3k scRNA-seq dataset: https://scanpy.scverse.org/en/stable/tutorials/basics/clustering-2017.html
     """
     data_path = pkg_resources.files(anchor='genesetgpt.data').joinpath('example_gene_set.txt')
     with data_path.open(mode='r', encoding='utf-8') as file:
